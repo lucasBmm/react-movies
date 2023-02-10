2da6d6fc4d695303241d9fad840a81a8d22af7af
Lucas Bomfim Mujo de Mattos <lucas.bomfim@bndes.gov.br>
2023-02-10 14:51:49 -0300

    ajuste tela movies

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,5 +1,6 @@
-import { MovieResult } from 'moviedb-promise';
+import { MovieResult, PopularMoviesResponse } from 'moviedb-promise';
 import React, { useEffect, useState } from 'react';
+import { DropdownItemProps, Input, Pagination, Select } from 'semantic-ui-react';
 import { api } from '../../App';
 import { useAppSelector } from '../../app/hooks';
 import { configSelector } from '../../features/defaultConfig';
@@ -11,28 +12,73 @@ import styles from './Movies.module.scss';
 
 export function Movies() {
     const [ movies, setMovies ] = useState<MovieResult[]>([]);
+    const [ pages, setPages ] = useState<PopularMoviesResponse>();
+    const [ selected, setSelected ] = useState<string>('popularidade');
     const configState = useAppSelector(configSelector);
 
-    const getMovies = () => {
-        api.moviePopular({ language: getUserLanguage(), region: country.Brazil }).then(res => {
-          if (res.results) setMovies(res.results);
-        });
+    const options: DropdownItemProps[] = [
+        { key: 'popularidade', text: 'Popularidade', value: 'popularidade' },
+        { key: 'avaliados', text: 'Melhor avaliados', value: 'avaliados' },
+        { key: 'reviews', text: 'Com mais Reviews', value: 'reviews' },
+    ];
+
+    const getMovies = (page: number) => {
+        setMovies([]);
+
+        switch (selected) {
+            case 'popularidade': {
+                api.moviePopular({ language: getUserLanguage(), region: country.Brazil, page }).then(res => {
+                  if (res.results) setMovies(res.results);
+                  setPages({...res, results: []});
+                });
+                break;
+            }
+
+            case 'avaliados': {
+                api.movieTopRated({ language: getUserLanguage(), region: country.Brazil, page }).then(res => {
+                    if (res.results) setMovies(res.results);
+                    setPages({...res, results: []});
+                });
+                break;
+            }
+
+            default: {
+                alert("Erro")
+            }
+        }
+
     }
 
     useEffect(() => {
-        getMovies();
+        getMovies(1);
     }, []);
+
+    useEffect(() => {
+        getMovies(1);
+    }, [selected]);
 
     return (
         <Layout>
             <div className={styles.main_container}>
                 <div className={styles.filters_container}>
-
+                    <label htmlFor=""></label>
+                    <Input className='' icon='search' placeholder='Search...' />
+                    <Select options={options} defaultValue='popularidade' onChange={(e, d) => setSelected(d.value as string)} />
                 </div>
                 <div className={styles.movies_container}>
-                    {movies.map((movie, index) => (
-                        <CardComponent key={index} src={`${configState?.images.base_url}w500${movie.poster_path}`} date={''} name={movie.title!} id={movie.id!} />
-                    ))}
+                    <div className={styles.movies_list}>
+                        {movies.map((movie, index) => (
+                            <CardComponent key={index} src={`${configState?.images.base_url}w500${movie.poster_path}`} date={''} name={movie.title!} id={movie.id!} />
+                        ))}
+                    </div>
+                    <div className={styles.pagination_container}>
+                        {pages && 
+                            <Pagination 
+                                defaultActivePage={1} 
+                                totalPages={`${pages?.total_pages! > 500 ? 500 : pages?.total_pages}`} 
+                                onPageChange={e => getMovies(Number(e.currentTarget?.innerText)) }
+                        />}
+                    </div>
                 </div>
             </div>
         </Layout>
